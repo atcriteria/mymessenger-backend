abb8992d57df5c9ae1883d2e9bb51b5ed015ccd9
dude lao <brycelipscomb@gmail.com>
2021-08-02 04:27:05 -0400

    fleshed out the server to read the usernames of connecting users and print them in the console for now. Added implementation for comparing a connecting user to a banned user registry in the event that I want to add moderation to this

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = require('http').createServer(app);
+// Need the cors statement to accept connections
+// from our localhost connections.
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
 const PORT = process.env.PORT || 3333
 
 // We don't need this but I'll leave it in for now
@@ -11,13 +17,19 @@ app.get('/', (req, res) => {
 
 // socket.io listens for a 'connection' type and 
 // logs to let us know someone connected
-io.on('connection', (socket) => {
-    socket.on('new-operations', (data) =>{
-        io.emit("new-remote-operations", data)
-    })
-    console.log("A user connected")
+io.use((socket, next) => {
+    const username=socket.handshake.auth.username;
+    if (!username){
+        return next(new Error("invalid username"))
+    }
+    // setting this up for instances where we want to 
+    // ban users in the future
+    if (username === "badUser"){
+        return next(new Error("invalid username"))
+    }
+    console.log(`${username} has connected`)
 })
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`\nServer listening on port: ${PORT}\n`)
 })
