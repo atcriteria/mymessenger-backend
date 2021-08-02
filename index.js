d334d89993281b70ed7002c6f5b129d3b51a032d
dude lao <brycelipscomb@gmail.com>
2021-08-01 21:33:12 -0400

    fleshed out the express server with socket.io functionality to listen for connections

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3333
+
+// We don't need this but I'll leave it in for now
+// just to have some output in the browser
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+})
+
+// socket.io listens for a 'connection' type and 
+// logs to let us know someone connected
+io.on('connection', (socket) => {
+    console.log("A user connected")
+})
+
+http.listen(PORT, () => {
+    console.log(`\nServer listening on port: ${PORT}\n`)
+})
